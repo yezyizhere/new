e3e6c95c9297927792929b35547aa31b0240526f
MIDZY <itmidnal0212@gmail.com>
2025-06-10 17:12:05 +0000

    mop

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,19 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '@/pages/HomeView.vue'
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
-    { path: '/', name: 'home', component: HomeView },
-    { path: '/test', name: 'test', component: () => import('@/pages/Test.vue') },
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: () => import('@/pages/Test.vue'),
+    },
   ],
 })
 
